9c1e17a8d631cd91ed827ac187556d13c1a2ce80
Ify Ndu <ify@Ifys-MacBook-Pro.local>
2018-09-27 00:14:07 +0100

    - Ejected react in order to access babelrc and add other configurations. - Modified common directory containig store - reducers, actions and middlewares are all exported from their various index.js files - modular css - modified non class functions to arrow styled functions - will set up babel modle resolver for use with aliases

--- web/src/components/app/App.js
+++ web/src/components/app/App.js
@@ -15,7 +15,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to Versus Root</h1>
+          <h1 className="App-title">Welcome to Versus Rooter</h1>
         </header>
         <p className="App-intro">
           {leagues}
--- web/src/router.js
+++ web/src/router.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import App from './components/App/App';
+import App from './components/app/App';
 import UploadGame from './components/UploadGame/UploadGame';
 
 export default (
+  
   <Router>
-    <div className='Router__container'>
-      <Switch location={window.location}>
-        <Route exact path={`${process.env.PUBLIC_URL}/`} component={App}/>
-        <Route exact path={`${process.env.PUBLIC_URL}/upload-game`} component={UploadGame}/>
-        <Route render={() => <div>Versus - Not Found</div>} />
-      </Switch>
-    </div>
+    <Switch location={window.location}>
+      <Route exact path={`${process.env.PUBLIC_URL}/`} component={App} />
+      <Route exact path={`${process.env.PUBLIC_URL}/upload-game`} component={UploadGame} />
+      <Route render={() => <div>Versus - Not Found</div>} />
+    </Switch>
   </Router>
 )
